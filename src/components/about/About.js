294d4fffeaadebf919f36dd8c4882d2222977c33
Av1nay <avi9071@gmail.com>
2022-10-17 14:40:14 +0300

    about section added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import './about.css'
+import Avatar from '../../assets/avatar1.png'
+import {GiAchievement} from 'react-icons/gi'
+import {IoIosPeople} from 'react-icons/io'
+import {AiFillProject} from 'react-icons/ai'
 
 const About = () =>{
     return(
-        <section id='about'>About</section>
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+            <div className="conatainer about_container">
+                <div className="about_me">
+                    <div className="about_me_image">
+                        <img src={Avatar} alt="profile pic" />
+                    </div>
+                </div>    
+            
+                <div className="about_content">
+                    <div className="about_cards">
+                        <article className="about_card">
+                            <GiAchievement className='icons'/>
+                            <h5>Experience</h5>
+                            <small>1+ Years of Working</small>
+                        </article>
+                        <article className="about_card">
+                            <IoIosPeople className='icons'/>
+                            <h5>Clients</h5>
+                            <small>more than 100</small>
+                        </article>
+                        <article className="about_card">
+                            <AiFillProject className='icons'/>
+                            <h5>Projects</h5>
+                            <small>More than 200+ Projects</small>
+                        </article>
+                    </div>
+                    <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea placeat nisi explicabo doloremque, tempore corporis dolores deleniti iusto soluta tempora mollitia maxime. Iste minima voluptate minus nostrum. Minima, asperiores temporibus.
+                        </p>
+                        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 

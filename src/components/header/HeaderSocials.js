88ebbe699a78d77def1df4ada10d9ad11d626d2e
Av1nay <avi9071@gmail.com>
2022-11-12 23:41:39 +0200

    traget black removed

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -4,8 +4,8 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/abhinay-chaudhary-822180b4/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/Av1nay" target="_blank"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/abhinay-chaudhary-822180b4/" ><BsLinkedin/></a>
+        <a href="https://github.com/Av1nay" ><BsGithub/></a>
     </div>
   )
 }
